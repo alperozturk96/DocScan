87098d7338363cd1610a5b02bcbab7b68402cb08
alperozturk <alper_ozturk@proton.me>
2024-03-15 15:37:32 +0100

    Init project

---
+++ app/src/main/java/com/coolnexttech/docscan/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.coolnexttech.docscan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.coolnexttech.docscan.ui.theme.DocScanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DocScanTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text("Android")
+                }
+            }
+        }
+    }
+}
