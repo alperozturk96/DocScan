659eb4cfc78703f672e75e5465e8ce4ba25334da
alperozturk <alper_ozturk@proton.me>
2024-03-15 15:39:13 +0100

    Init project
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,5 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 }
