11ec71ec06ec263d560db1498988639a922de2ab
alperozturk <alper_ozturk@proton.me>
2024-06-24 15:13:44 +0200

    Fixes
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/coolnexttech/docscan/ui/scanner/ScannerScreen.kt
+++ app/src/main/java/com/coolnexttech/docscan/ui/scanner/ScannerScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
