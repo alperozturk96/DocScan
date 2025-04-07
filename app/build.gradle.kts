881dde7540cd1297845568b14e46988afe7b1084
alperozturk <alper_ozturk@proton.me>
2025-04-07 21:56:32 +0200

    Fixes
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.coolnexttech.docscan"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.2"
 
         vectorDrawables {
